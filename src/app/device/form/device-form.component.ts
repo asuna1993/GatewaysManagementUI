0ee3b369489753fde7300b60ecca11a91394a6da
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2024-08-15 14:46:11 -0400

    perf: implement OnDestroy interface and unsubscribe from the ActivatedRoute subscription to prevent memory leaks

--- src/app/device/form/device-form.component.ts
+++ src/app/device/form/device-form.component.ts
@@ -1,14 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { DeviceDTO } from '../device';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-device-form',
   templateUrl: './device-form.component.html',
   styleUrls: ['./device-form.component.css']
 })
-export class DeviceFormComponent implements OnInit {
+export class DeviceFormComponent implements OnInit, OnDestroy {
+  private routeSubscription: Subscription;
 
   constructor(private formBuilder: FormBuilder, 
     private activatedRoute: ActivatedRoute) { }
@@ -26,7 +28,7 @@ export class DeviceFormComponent implements OnInit {
   onSubmit: EventEmitter<DeviceDTO> = new EventEmitter<DeviceDTO>();
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params => {
+    this.routeSubscription = this.activatedRoute.params.subscribe(params => {
       this.gatewayId = params['gatewayId'];
     });
 
@@ -46,13 +48,19 @@ export class DeviceFormComponent implements OnInit {
       this.form.patchValue(this.model);
     }
   }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
+  }
   
   submitForm(){
+    if (this.form.valid) {
        this.onSubmit.emit(this.form.value);
+    }
   }
 
-  GetFieldError(fieldName: string){
-    var field = this.form.get(fieldName);
+  GetFieldError(fieldName: string): string{
+    const field = this.form.get(fieldName);
     if(field?.hasError('required')){
       return 'The field is required';
     }
